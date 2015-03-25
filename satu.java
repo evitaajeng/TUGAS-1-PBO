aaf1caac3207fd3c84d6f832d0689b591fd5a88b
evitaajeng <evitaajeng@gmail.com>
2015-03-25 18:13:39 +0700

    Pertama
    Mengecek tinggi badan

---
+++ satu.java
@@ -0,0 +1,13 @@
+/**
+ * Created by TOSHIBA on 3/25/2015.
+ */
+public class satu {
+    public static void main(String[] args) {
+        float tinggi = 110;
+        if (tinggi < 100) {
+            System.out.println("Tinggi tidak memenuhi syarat");
+        } else {
+            System.out.println("Tinggi memenuhi syarat");
+        }
+    }
+}
