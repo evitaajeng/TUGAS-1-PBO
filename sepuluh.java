329fc1e0afb19fec4306133e7293517b5c11f229
evitaajeng <evitaajeng@gmail.com>
2015-03-25 18:29:01 +0700

    kesepuluh

---
+++ sepuluh.java
@@ -0,0 +1,16 @@
+/**
+ * Created by TOSHIBA on 3/25/2015.
+ */
+public class sepuluh {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        int z=4;
+        for(x=1;x<=z;x++){
+            for(y=1;y<=z;y++){
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+    }
+}
