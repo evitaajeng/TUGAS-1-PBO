1117c32eecd5fad96d5fed86dcb1580e684b2f27
evitaajeng <evitaajeng@gmail.com>
2015-03-25 18:29:47 +0700

    kesebelas

---
+++ sebelas.java
@@ -0,0 +1,17 @@
+public class sebelas {
+	public static void main(String[] args) {
+		System.out.println(" ");
+		int g;
+		int h;
+		int i=5;
+		for (g=1;g<=i;g++){
+			if(g%2==0){
+				System.out.print(" ");
+			}
+			for(h=1;h<=i;h++){
+				System.out.print('#');
+			}
+			System.out.print("\n");
+		}
+	}
+}
